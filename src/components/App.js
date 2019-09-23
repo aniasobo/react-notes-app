3926ec6c15e5e07420871a0098d3f053f0ae1e92
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-09-23 15:00:49 +0100

    Add state, setState and onClick to form

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import { Form, FormControl, Button } from 'react-bootstrap'
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      text: ''
+    }
+  }
+
   render() {
     return (
       <div>
         <h2>Notes</h2>
           <Form>
-            <FormControl /> 
-            <Button>Add note</Button>
+            <FormControl onChange={event => this.setState({ text: event.target.value })} /> 
+            <Button onClick={() => console.log(this.state)}>Add note</Button>
           </Form>
       </div>
     )

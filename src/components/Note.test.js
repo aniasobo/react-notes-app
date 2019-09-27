4b93704a1e314404470e39105c4447f5540a19a8
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-09-27 14:59:53 +0100

    Add first passing test

---
+++ src/components/Note.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { mount } from 'enzyme'; // mimic the mounting of the component onto the DOM
+import Note from './Note';
+import { isTSAnyKeyword, exportAllDeclaration } from '@babel/types';
+
+const props = { note: { text: 'test text' }}
+
+describe('Note', () => {
+  let note = mount(<Note {...props}/>);
+
+  it('renders the test text', () => {
+   // console.log(note.debug()); a bit like pry
+    expect(note.find('span').text()).toEqual(props.note.text);
+  });
+})
